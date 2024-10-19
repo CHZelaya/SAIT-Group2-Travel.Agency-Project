0f2155ec8d23e2d849bef36a671d34ceb157a8d9
Princcy <princcy4real@yahoo.com>
2024-10-19 06:16:31 -0600

    Fixed the issue of current date not populating automatically on the order form

--- public/js/orderscript.js
+++ public/js/orderscript.js
@@ -1,15 +1,15 @@
 
 //automatically prompt current date
-function getCurrentDate() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // 
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
+// // function getCurrentDate() {
+// //     const date = new Date();
+// //     const year = date.getFullYear();
+// //     const month = String(date.getMonth() + 1).padStart(2, '0'); // 
+// //     const day = String(date.getDate()).padStart(2, '0');
+// //     return `${year}-${month}-${day}`;
+// }
 
 
-document.getElementById('currentDate').value = getCurrentDate();
+// document.getElementById('currentDate').value = getCurrentDate();
 
 //generates random Booking ID
 function generateRandomString() {
